d2a7cc2ce24b8a7b895317109bab6196d760a344
George G. Vega Yon <xrd4@cdc.gov>
2024-03-14 15:02:37 -0600

    Added tests for poisson and negbinom obs + more doc

--- model/src/pyrenew/observations/negativebinomial.py
+++ model/src/pyrenew/observations/negativebinomial.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 
-"""
-Observation helper classes
-"""
+
+import numbers as nums
 
 import numpyro
 import numpyro.distributions as dist
+from numpy.typing import ArrayLike
 from pyrenew.metaclasses import RandomProcess
 
 
 class NegativeBinomialObservation(RandomProcess):
+    """Negative Binomial observation"""
+
     def __init__(
         self,
-        concentration_prior: dist.Distribution,
+        concentration_prior: dist.Distribution | ArrayLike,
         concentration_suffix: str = "_concentration",
         parameter_name="negbinom_rv",
     ):
-        """
-        Default constructor
+        """Default constructor
 
-        Parameters
-        ----------
-        concentration_prior : dist.Distribution
+        :param concentration_prior: dist.Distribution
             Numpyro distribution from which to
             sample the positive concentration
             parameter of the negative binomial.
@@ -33,25 +32,51 @@ class NegativeBinomialObservation(RandomProcess):
             imply that the distribution becomes
             more Poissonian, while smaller ones
             imply a greater degree of dispersion.
+        :type concentration_prior: dist.Distribution
+        :param concentration_suffix: _description_, defaults to "_concentration"
+        :type concentration_suffix: str, optional
+        :param parameter_name: _description_, defaults to "negbinom_rv"
+        :type parameter_name: str, optional
         """
-        self.concentration_prior = concentration_prior
+
+        self.validate(concentration_prior)
+
+        if isinstance(concentration_prior, dist.Distribution):
+            self.sample_prior = lambda: numpyro.sample(
+                self.parameter_name + self.concentration_suffix,
+                concentration_prior,
+            )
+        else:
+            self.sample_prior = lambda: concentration_prior
+
         self.parameter_name = parameter_name
         self.concentration_suffix = concentration_suffix
 
-    def sample(self, predicted_value, data=None, obs=None):
-        concentration_parameter = numpyro.sample(
-            self.parameter_name + self.concentration_suffix,
-            self.concentration_prior,
-        )
+    def sample(
+        self, predicted_value: ArrayLike, data: dict = dict(), obs=None
+    ):
+        """Sample from the negative binomial distribution
 
+        :param predicted_value: Mean parameter of the negative binomial
+        :type predicted_value: ArrayLike
+        :param data: Ignored, defaults to dict()
+        :type data: dict, optional
+        :param obs: _description_, defaults to None
+        :type obs: _type_, optional
+        :return: _description_
+        :rtype: _type_
+        """
         return numpyro.sample(
             self.parameter_name,
             dist.NegativeBinomial2(
-                mean=predicted_value, concentration=concentration_parameter
+                mean=predicted_value, concentration=self.sample_prior()
             ),
             obs=obs,
         )
 
     @staticmethod
-    def validate():
+    def validate(concentration_prior) -> None:
+        assert isinstance(
+            concentration_prior, (dist.Distribution, nums.Number)
+        )
         return None
--- model/src/pyrenew/observations/poisson.py
+++ model/src/pyrenew/observations/poisson.py
@@ -3,6 +3,7 @@
 
 import numpyro
 import numpyro.distributions as dist
+from numpy.typing import ArrayLike
 from pyrenew.metaclasses import RandomProcess
 
 
@@ -12,10 +13,31 @@ class PoissonObservation(RandomProcess):
     """
 
     def __init__(self, parameter_name="poisson_rv") -> None:
+        """Constructor
+
+        :param parameter_name: Passed to numpyro.sample, defaults to
+            "poisson_rv"
+        :type parameter_name: str, optional
+        :return: _description_
+        :rtype: _type_
+        """
+
         self.parameter_name = parameter_name
         return None
 
-    def sample(self, predicted_value, data=None, obs=None):
+    def sample(self, predicted_value: ArrayLike, data: dict = None, obs=None):
+        """Sample from the Poisson process
+
+        :param predicted_value: Rate parameter passed to
+            numpyro.distributions.Poisson.
+        :type predicted_value: ArrayLike
+        :param data: Ignored, defaults to None
+        :type data: _type_, optional
+        :param obs: Observed data passed to numpyro.sample, defaults to None
+        :type obs: _type_, optional
+        :return: _description_
+        :rtype: _type_
+        """
         return numpyro.sample(
             self.parameter_name, dist.Poisson(rate=predicted_value), obs=obs
         )
