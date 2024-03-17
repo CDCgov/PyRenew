16c3dd6d76b12acd73a45d3c3906b61a7588598e
R543hfer099JJfd <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-03-17 16:36:46 -0400

    Setting up Sphinx site again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,14 +5,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-# import pathlib
-# import sys
-# sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../model/src'))
-
 
 project = 'CFA Multisignal Renewal'
 copyright = '2024, CFA'
@@ -22,18 +14,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
-    'sphinx.ext.duration',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.inheritance_diagram',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.mathjax',
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
