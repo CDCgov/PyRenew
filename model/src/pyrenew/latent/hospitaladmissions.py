d8cca4258bd5c354de73fb14a7bce53f25bad8b8
George G. Vega Yon <xrd4@cdc.gov>
2024-03-21 12:38:46 -0600

    Copy doc from wastewater model on Hosp Admin to class

--- model/src/pyrenew/latent/hospitaladmissions.py
+++ model/src/pyrenew/latent/hospitaladmissions.py
@@ -18,15 +18,45 @@ HospAdmissionsSample = namedtuple(
 
 
 class HospitalAdmissions(RandomVariable):
-    """Latent hospital admissions"""
+    """Latent hospital admissions
+
+    Implements a renewal process for the expected number of hospitalizations.
+
+    Notes
+    -----
+
+    The following text was directly extracted from the wastewater model
+    documentation
+    (`link <https://github.com/cdcent/cfa-forecast-renewal-ww/blob/a17efc090b2ffbc7bc11bdd9eec5198d6bcf7322/model_definition.md#hospital-admissions-component> `_).
+
+    Following other semi-mechanistic renewal frameworks, we model the _expected_
+    hospital admissions per capita :math:`H(t)` as a convolution of the
+    _expected_ latent incident infections per capita :math:`I(t)`, and a
+    discrete infection to hospitalization distribution :math:`d(\tau)`, scaled
+    by the probability of being hospitalized :math:`p_\mathrm{hosp}(t)`.
+
+    To account for day-of-week effects in hospital reporting, we use an
+    estimated _weekday effect_ :math:`\omega(t)`. If :math:`t` and :math:`t'`
+    are the same day of the week, :math:`\omega(t) = \omega(t')`. The seven
+    values that :math:`\omega(t)` takes on are constrained to have mean 1.
+
+    .. math::
+
+        H(t) = \omega(t) p_\mathrm{hosp}(t) \sum_{\tau = 0}^{T_d} d(\tau) I(t-\tau)
+
+    Where :math:`T_d` is the maximum delay from infection to hospitalization
+    that we consider.
+    """
 
     def __init__(
         self,
         inf_hosp_int: ArrayLike,
         infections_varname: str = "infections",
-        IHR_obs_varname: str = "IHR",
+        infect_hosp_rate_varname: str = "IHR",
         hospitalizations_predicted_varname: str = "predicted_hospitalizations",
-        IHR_dist: dist.Distribution = dist.LogNormal(jnp.log(0.05), 0.05),
+        infect_hosp_rate_dist: dist.Distribution = dist.LogNormal(
+            jnp.log(0.05), 0.05
+        ),
     ) -> None:
         """Default constructor
 
@@ -37,33 +67,34 @@ class HospitalAdmissions(RandomVariable):
         infections_varname : str
             Name of the entry in random_variables that holds the vector of
             infections.
-        IHR_obs_varname : str
-            Name of the entry in random_variables that holds the observed IHR
+        infect_hosp_rate_varname : str
+            Name of the entry in random_variables that holds the observed
+            infection-hospitalization rate (IHR).
             (if available).
         hospitalizations_predicted_varname : str
             Name to assign to the deterministic component in numpyro of
             predicted hospitalizations.
-        IHR_dist : dist.Distribution, optional
+        infect_hosp_rate_dist : dist.Distribution, optional
             Infection to hospitalization rate pmf.
 
         Returns
         -------
         None
         """
-        HospitalAdmissions.validate(IHR_dist)
+        HospitalAdmissions.validate(infect_hosp_rate_dist)
 
-        self.IHR_obs_varname = IHR_obs_varname
+        self.infect_hosp_rate_varname = infect_hosp_rate_varname
         self.infections_varname = infections_varname
         self.hospitalizations_predicted_varname = (
             hospitalizations_predicted_varname
         )
 
-        self.IHR_dist = IHR_dist
+        self.infect_hosp_rate_dist = infect_hosp_rate_dist
         self.inf_hosp = validate_discrete_dist_vector(inf_hosp_int)
 
     @staticmethod
-    def validate(IHR_dist) -> None:
-        assert isinstance(IHR_dist, dist.Distribution)
+    def validate(infect_hosp_rate_dist) -> None:
+        assert isinstance(infect_hosp_rate_dist, dist.Distribution)
 
         return None
 
@@ -95,8 +126,8 @@ class HospitalAdmissions(RandomVariable):
 
         IHR = npro.sample(
             "IHR",
-            self.IHR_dist,
-            obs=random_variables.get(self.IHR_obs_varname, None),
+            self.infect_hosp_rate_dist,
+            obs=random_variables.get(self.infect_hosp_rate_varname, None),
         )
 
         IHR_t = IHR * random_variables.get(self.infections_varname)
