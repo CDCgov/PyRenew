338dee06c826a7da5dcc0c09ff5ca27d96763db1
George G. Vega Yon <xrd4@cdc.gov>
2024-04-11 15:17:13 -0600

    Using os.path.exist

--- model/src/pyrenew/datasets/data-raw/generation_interval.py
+++ model/src/pyrenew/datasets/data-raw/generation_interval.py
@@ -1,13 +1,25 @@
 # Dataset from:
 # https://raw.githubusercontent.com/CDCgov/wastewater-informed-covid-forecasting/0962c5d1652787479ac72caebf076ab55fe4e10c/input/saved_pmfs/generation_interval.csv
+import os
+
 import polars as pl
 
 gen_int = pl.read_csv(
     "https://raw.githubusercontent.com/CDCgov/wastewater-informed-covid-forecasting/0962c5d1652787479ac72caebf076ab55fe4e10c/input/saved_pmfs/generation_interval.csv",
 )
 
+# Building path to save the file
+path = os.path.join(
+    "src",
+    "pyrenew",
+    "datasets",
+    "gen_int.tsv",
+)
+
+assert os.path.exists(os.path.dirname(path))
+
 gen_int.write_csv(
-    file="src/pyrenew/datasets/gen_int.tsv",
+    file=path,
     separator="\t",
     include_header=True,
     null_value="",
--- model/src/pyrenew/datasets/data-raw/infection_admission_interval.py
+++ model/src/pyrenew/datasets/data-raw/infection_admission_interval.py
@@ -1,14 +1,26 @@
 # Dataset from:
 # https://raw.githubusercontent.com/CDCgov/wastewater-informed-covid-forecasting/0962c5d1652787479ac72caebf076ab55fe4e10c/input/saved_pmfs/inf_to_hosp.csv
+import os
+
 import polars as pl
 
 # Read CSV file from URL
 url = "https://raw.githubusercontent.com/CDCgov/wastewater-informed-covid-forecasting/0962c5d1652787479ac72caebf076ab55fe4e10c/input/saved_pmfs/inf_to_hosp.csv"
 infection_admission_interval = pl.read_csv(url)
 
+# Building path to save the file
+path = os.path.join(
+    "src",
+    "pyrenew",
+    "datasets",
+    "infection_admission_interval.tsv",
+)
+
+assert os.path.exists(os.path.dirname(path))
+
 # Save as TSV
 infection_admission_interval.write_csv(
-    file="src/pyrenew/datasets/infection_admission_interval.tsv",
+    file=path,
     separator="\t",
     include_header=True,
     null_value="",
