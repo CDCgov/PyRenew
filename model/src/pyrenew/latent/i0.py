a9b73d832168e2d08c9c471493e467c15f9537cc
upx3—TM (CFA) <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-04-15 14:56:47 -0400

    First Pass Documentation (docstrings) For Latent Folder (#73)
    * numpydoc changes (testing)
    
    * numpydoc changes (testing)
    
    * docstrings added for latent hospitaladmissions
    
    * latent hospitaladmissions edit; I0 docstrings
    
    * infection functions minor docs edits
    
    * infections docs edit; latent folder first pass
    
    * latent folder precommit
    
    * poetry lock for latent folder
    
    * typing typo fixed
    
    * typing typo fixed
    
    * Poetry group for development
    
    * Update docs.yaml
    
    ---------
    
    Co-authored-by: George G. Vega Yon <xrd4@cdc.gov>
    Co-authored-by: George G. Vega Yon <g.vegayon@gmail.com>

--- model/src/pyrenew/latent/i0.py
+++ model/src/pyrenew/latent/i0.py
@@ -1,3 +1,5 @@
+from typing import Any, Optional
+
 import numpyro as npro
 import numpyro.distributions as dist
 from pyrenew.metaclass import RandomVariable
@@ -8,12 +10,19 @@ class Infections0(RandomVariable):
 
     It creates a random variable for the initial infections with a prior
     distribution.
+
+    Methods
+    -------
+    validate(i0_dist)
+        Validate the initial infections distribution.
+    sample(**kwargs)
+        Sample the initial infections.
     """
 
     def __init__(
         self,
-        name: str = "I0",
-        I0_dist: dist.Distribution = dist.LogNormal(0, 1),
+        name: Optional[str] = "I0",
+        I0_dist: Optional[dist.Distribution] = dist.LogNormal(0, 1),
     ) -> None:
         """Default constructor
 
@@ -36,17 +45,22 @@ class Infections0(RandomVariable):
         return None
 
     @staticmethod
-    def validate(i0_dist):
+    def validate(i0_dist: Any):
         """Validate the initial infections distribution.
 
         Parameters
         ----------
-        i0_dist : dist.Distribution
-            Distribution of the initial infections.
+        i0_dist : Any
+            Distribution (expected dist.Distribution) of the initial infections.
 
         Returns
         -------
         None
+
+        Raises
+        ------
+        AssertionError
+            If the inputted distribution is not a Numpyro distribution.
         """
         assert isinstance(i0_dist, dist.Distribution)
 
@@ -59,7 +73,8 @@ class Infections0(RandomVariable):
         Parameters
         ----------
         **kwargs : dict, optional
-            Ignored
+            Additional keyword arguments passed through to internal
+            sample calls, should there be any.
 
         Returns
         -------
