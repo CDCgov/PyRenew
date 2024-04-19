9ff6b11e2cc979112c660f71ea9b2a0f9b4379ff
George G. Vega Yon <g.vegayon@gmail.com>
2024-04-19 13:08:03 -0600

    Update model/src/pyrenew/datasets/data-raw/wastewater.R
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

--- model/src/pyrenew/datasets/data-raw/wastewater.R
+++ model/src/pyrenew/datasets/data-raw/wastewater.R
@@ -1,6 +1,8 @@
 load(
   "https://github.com/CDCgov/wastewater-informed-covid-forecasting/blob/292526383ece582f10823fc939c7e590ca349c6d/cfaforecastrenewalww/data/example_df.rda"
   )
+datasets_dir <- file.path("src", "pyrenew", "datasets")
+dir.create(datasets_dir)
 
 # Saving as TSV
 write.table(
