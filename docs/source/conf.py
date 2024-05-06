e261f16fd6233e3caf0706abf0938db846eceef6
George G. Vega Yon <g.vegayon@gmail.com>
2024-05-06 15:01:47 -0600

    Changing the theme of sphinx (#104)
    * Changing the theme of sphinx
    
    * Updating poetry in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,6 +55,6 @@ todo_include_todos = False
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "classic"
+html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_css_files = ["msei.css"]
