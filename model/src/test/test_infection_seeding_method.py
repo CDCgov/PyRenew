18c828712da59002bf1eeb4417be47b4d8de03ea
damonbayer <xum8@cdc.gov>
2024-06-03 14:32:06 -0500

    add testing from InfectionSeedMethods and InfectionSeedProcess

---
+++ model/src/test/test_infection_seeding_method.py
@@ -0,0 +1,77 @@
+# numpydoc ignore=GL08
+import numpy as np
+import numpy.testing as testing
+from pyrenew.deterministic import DeterministicVariable
+from pyrenew.latent import (
+    SeedInfectionsExponential,
+    SeedInfectionsFromVec,
+    SeedInfectionsZeroPad,
+)
+
+
+def test_seed_infections_exponential():
+    """Check that the SeedInfectionsExponential class generates the correct number of infections at each time point."""
+    n_timepoints = 10
+    rate_RV = DeterministicVariable(0.5)
+    I_pre_seed_RV = DeterministicVariable(10.0)
+    default_t_pre_seed = n_timepoints - 1
+
+    (I_pre_seed,) = I_pre_seed_RV.sample()
+    (rate,) = rate_RV.sample()
+
+    infections_default_t_pre_seed = SeedInfectionsExponential(
+        n_timepoints, rate=rate_RV
+    ).seed_infections(I_pre_seed)
+    infections_default_t_pre_seed_manual = I_pre_seed * np.exp(
+        rate * (np.arange(n_timepoints) - default_t_pre_seed)
+    )
+
+    testing.assert_array_equal(
+        infections_default_t_pre_seed, infections_default_t_pre_seed_manual
+    )
+
+    # assert that infections at default t_pre_seed is I_pre_seed
+    assert infections_default_t_pre_seed[default_t_pre_seed] == I_pre_seed
+
+    t_pre_seed = 6
+    infections_custom_t_pre_seed = SeedInfectionsExponential(
+        n_timepoints, rate=rate_RV, t_pre_seed=t_pre_seed
+    ).seed_infections(I_pre_seed)
+    infections_custom_t_pre_seed_manual = I_pre_seed * np.exp(
+        rate * (np.arange(n_timepoints) - t_pre_seed)
+    )
+    testing.assert_array_equal(
+        infections_custom_t_pre_seed, infections_custom_t_pre_seed_manual
+    )
+
+    assert infections_custom_t_pre_seed[t_pre_seed] == I_pre_seed
+    return None
+
+
+def test_seed_infections_zero_pad():
+    """Check that the SeedInfectionsZeroPad class generates the correct number of infections at each time point."""
+
+    n_timepoints = 10
+    I_pre_seed_RV = DeterministicVariable(10.0)
+    (I_pre_seed,) = I_pre_seed_RV.sample()
+
+    infections = SeedInfectionsZeroPad(n_timepoints).seed_infections(
+        I_pre_seed
+    )
+    testing.assert_array_equal(
+        infections, np.pad(I_pre_seed, (n_timepoints - I_pre_seed.size, 0))
+    )
+    return None
+
+
+def test_seed_infections_from_vec():
+    """Check that the SeedInfectionsFromVec class generates the correct number of infections at each time point."""
+    n_timepoints = 10
+    I_pre_seed_RV = DeterministicVariable(np.arange(10))
+    (I_pre_seed,) = I_pre_seed_RV.sample()
+
+    infections = SeedInfectionsFromVec(n_timepoints).seed_infections(
+        I_pre_seed
+    )
+    testing.assert_array_equal(infections, I_pre_seed)
+    return None
---
+++ model/src/test/test_infection_seeding_process.py
@@ -0,0 +1,38 @@
+# numpydoc ignore=GL08
+import jax.numpy as jnp
+import numpyro as npro
+import numpyro.distributions as dist
+from pyrenew.deterministic import DeterministicVariable
+from pyrenew.latent import (
+    InfectionSeedingProcess,
+    SeedInfectionsExponential,
+    SeedInfectionsFromVec,
+    SeedInfectionsZeroPad,
+)
+from pyrenew.metaclass import DistributionalRV
+
+
+def test_infection_seeding_process():
+    """Check that the InfectionSeedingProcess class generates can be sampled from with all InfectionSeedMethods."""
+    n_timepoints = 10
+
+    zero_pad_model = InfectionSeedingProcess(
+        DistributionalRV(dist=dist.LogNormal(0, 1), name="I0"),
+        SeedInfectionsZeroPad(n_timepoints),
+    )
+
+    exp_model = InfectionSeedingProcess(
+        DistributionalRV(dist=dist.LogNormal(0, 1), name="I0"),
+        SeedInfectionsExponential(n_timepoints, DeterministicVariable(0.5)),
+    )
+
+    vec_model = InfectionSeedingProcess(
+        DeterministicVariable(jnp.arange(n_timepoints)),
+        SeedInfectionsFromVec(n_timepoints),
+    )
+
+    for model in [zero_pad_model, exp_model, vec_model]:
+        with npro.handlers.seed(rng_seed=1):
+            model.sample()
+
+    return None
