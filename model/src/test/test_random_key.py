dd5de7e6b82762f11a2fbae5929aec409e9eb76f
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-06-17 17:09:17 -0400

    DRY a bit less for tests

--- model/src/test/test_random_key.py
+++ model/src/test/test_random_key.py
@@ -5,7 +5,6 @@ Ensures that models created with the same or
 with different random keys behave appropriately.
 """
 
-
 import jax.numpy as jnp
 import jax.random as jr
 import numpy as np
@@ -24,14 +23,7 @@ from pyrenew.observation import PoissonObservation
 from pyrenew.process import RtRandomWalkProcess
 
 
-def test_rng_keys_produce_correct_samples():
-    """
-    Tests that the random keys specified for
-    MCMC sampling produce appropriate
-    output if left to None or specified directly.
-    """
-
-    # set up singular epidemiological process
+def create_test_model():  # numpydoc ignore=GL08
     pmf_array = jnp.array([0.25, 0.25, 0.25, 0.25])
     gen_int = DeterministicPMF(pmf_array, name="gen_int")
     I0 = InfectionSeedingProcess(
@@ -46,85 +38,55 @@ def test_rng_keys_produce_correct_samples():
         Rt_transform=t.ExpTransform().inv,
         Rt_rw_dist=dist.Normal(0, 0.025),
     )
+    model = RtInfectionsRenewalModel(
+        I0_rv=I0,
+        gen_int_rv=gen_int,
+        latent_infections_rv=latent_infections,
+        infection_obs_process_rv=observed_infections,
+        Rt_process_rv=rt,
+    )
+    return model
+
+
+def sample_test_model(
+    test_model, observed_infections, rng_key
+):  # numpydoc ignore=GL08
+    test_model.run(
+        num_warmup=50,
+        num_samples=50,
+        data_observed_infections=observed_infections,
+        rng_key=rng_key,
+        mcmc_args=dict(progress_bar=True),
+    )
+
+
+def test_rng_keys_produce_correct_samples():
+    """
+    Tests that the random keys specified for
+    MCMC sampling produce appropriate
+    output if left to None or specified directly.
+    """
+
+    # set up singular epidemiological process
 
     # set up base models for testing
-    model_01 = RtInfectionsRenewalModel(
-        I0_rv=I0,
-        gen_int_rv=gen_int,
-        latent_infections_rv=latent_infections,
-        infection_obs_process_rv=observed_infections,
-        Rt_process_rv=rt,
-    )
-    model_02 = RtInfectionsRenewalModel(
-        I0_rv=I0,
-        gen_int_rv=gen_int,
-        latent_infections_rv=latent_infections,
-        infection_obs_process_rv=observed_infections,
-        Rt_process_rv=rt,
-    )
-    model_03 = RtInfectionsRenewalModel(
-        I0_rv=I0,
-        gen_int_rv=gen_int,
-        latent_infections_rv=latent_infections,
-        infection_obs_process_rv=observed_infections,
-        Rt_process_rv=rt,
-    )
-    model_04 = RtInfectionsRenewalModel(
-        I0_rv=I0,
-        gen_int_rv=gen_int,
-        latent_infections_rv=latent_infections,
-        infection_obs_process_rv=observed_infections,
-        Rt_process_rv=rt,
-    )
-    model_05 = RtInfectionsRenewalModel(
-        I0_rv=I0,
-        gen_int_rv=gen_int,
-        latent_infections_rv=latent_infections,
-        infection_obs_process_rv=observed_infections,
-        Rt_process_rv=rt,
-    )
+    model_01 = create_test_model()
+    model_02 = create_test_model()
+    model_03 = create_test_model()
+    model_04 = create_test_model()
+    model_05 = create_test_model()
 
     # sample only a single model and use that model's samples
     # as the observed_infections for the rest of the models
     with npro.handlers.seed(rng_seed=np.random.randint(1, 600)):
         model_01_samp = model_01.sample(n_timepoints_to_simulate=30)
 
-    # run each test model instance;
-    model_01.run(
-        num_warmup=50,
-        num_samples=50,
-        data_observed_infections=model_01_samp.observed_infections,
-        rng_key=jr.key(54),  # rng_key specified, instance 1
-        mcmc_args=dict(progress_bar=True),
-    )
-    model_02.run(
-        num_warmup=50,
-        num_samples=50,
-        data_observed_infections=model_01_samp.observed_infections,
-        rng_key=jr.key(54),  # rng_key specified, instance 2
-        mcmc_args=dict(progress_bar=True),
-    )
-    model_03.run(
-        num_warmup=50,
-        num_samples=50,
-        data_observed_infections=model_01_samp.observed_infections,
-        rng_key=None,  # rng_key None, instance 1
-        mcmc_args=dict(progress_bar=True),
-    )
-    model_04.run(
-        num_warmup=50,
-        num_samples=50,
-        data_observed_infections=model_01_samp.observed_infections,
-        rng_key=None,  # rng_key None, instance 2
-        mcmc_args=dict(progress_bar=True),
-    )
-    model_05.run(
-        num_warmup=50,
-        num_samples=50,
-        data_observed_infections=model_01_samp.observed_infections,
-        rng_key=jr.key(74),  # different, but specified rng_key
-        mcmc_args=dict(progress_bar=True),
-    )
+    # run test models with the different keys
+    models = [model_01, model_02, model_03, model_04, model_05]
+    rng_keys = [jr.key(54), jr.key(54), None, None, jr.key(74)]
+    obs_infections = [model_01_samp.observed_infections] * len(models)
+    for elt in list(zip(models, obs_infections, rng_keys)):
+        sample_test_model(*elt)
 
     # using same rng_key should get same run samples
     assert np.array_equal(
