2084b7051ee3749362fe084009fda20f8997a8d1
damonbayer <xum8@cdc.gov>
2024-07-09 16:53:12 -0500

    use python script for post-processing rst files

---
+++ docs/post_process_rst.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+
+def process_rst_file(rst_file):
+    # Read the content of the rst file
+    with open(rst_file, "r") as file:
+        content = file.read()
+
+    # Replace `.. code:: mermaid` with `.. mermaid::`
+    # .. mermaid::
+    modified_content = re.sub(
+        r"\.\.\ code::\ mermaid", ".. mermaid::", content
+    )
+
+    # Write the modified content back to the file
+    with open(rst_file, "w") as file:
+        file.write(modified_content)
+
+
+if __name__ == "__main__":
+    rst_file = sys.argv[1]
+    process_rst_file(rst_file)
