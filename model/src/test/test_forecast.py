d0b93bef873cb1c65cb71efa2e98de8f1479e6fc
Damon Bayer <xum8@cdc.gov>
2024-07-11 23:39:20 -0500

    Forecasting Interface (#241)
    * add arviz plot for posterior predictive
    
    * broken simplerandomwalk
    
    * slight simplification
    
    * downgrade jax and jaxlib
    
    * fix scan in simplerandomwalkprocess
    
    * add ppc_plot to hosp_model
    
    * test prior/posterior_predictive plots
    
    * Damon check in
    
    * add plot_ppc, plot_lm
    
    * fix tests
    
    * formatting
    
    * more formatting
    
    * fix plot_lm output issue
    
    * suggestion from code review
    
    Co-authored-by: Damon Bayer <xum8@cdc.gov>
    
    * Update example_with_datasets.qmd
    
    * change plot_lm kwargs
    
    * add details on plot_ppc, fig labels and titles
    
    * add figure descriptions
    
    * code review suggestions
    
    Co-authored-by: Damon Bayer <xum8@cdc.gov>
    
    * remove link from code
    
    * formatting
    
    * simplify models
    
    * rename hospital_admissions_model
    
    * fix bad merges
    
    * fix hospital_admissions_model.qmd
    
    * fix most tests
    
    * fix remaining tests
    
    * rename test variable
    
    * adjust padding handling
    
    * rename internal variables
    
    * formatting
    
    * correcting merge errors
    
    * remove print debug statements and unused variables
    
    * fix test
    
    * rename for clarity
    
    * cleanup variable names
    
    * fix figure
    
    * delete example_with_datasets
    
    * remove unused code
    
    * rename init in test_random_walk
    
    * clarify forecasting
    
    * update numpyro and jax
    
    * update numpyro and jax
    
    * enforce minimum version of numpyro and jax
    
    * try to enforce numpyro version again
    
    * add test_forecast
    
    * Update docs/source/tutorials/hospital_admissions_model.qmd
    
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sbidari <zib2@cdc.gov>
    Co-authored-by: Subekshya Bidari <37636707+sbidari@users.noreply.github.com>
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

---
+++ model/src/test/test_forecast.py
@@ -0,0 +1,78 @@
+# numpydoc ignore=GL08
+
+import jax.numpy as jnp
+import jax.random as jr
+import numpy as np
+import numpyro as npro
+import numpyro.distributions as dist
+import pyrenew.transformation as t
+from numpy.testing import assert_array_equal
+from pyrenew.deterministic import DeterministicPMF
+from pyrenew.latent import (
+    Infections,
+    InfectionSeedingProcess,
+    SeedInfectionsZeroPad,
+)
+from pyrenew.metaclass import DistributionalRV
+from pyrenew.model import RtInfectionsRenewalModel
+from pyrenew.observation import PoissonObservation
+from pyrenew.process import RtRandomWalkProcess
+
+
+def test_forecast():
+    """Check that forecasts are the right length and match the posterior up until forecast begins."""
+    pmf_array = jnp.array([0.25, 0.25, 0.25, 0.25])
+    gen_int = DeterministicPMF(pmf_array, name="gen_int")
+    I0 = InfectionSeedingProcess(
+        "I0_seeding",
+        DistributionalRV(dist=dist.LogNormal(0, 1), name="I0"),
+        SeedInfectionsZeroPad(n_timepoints=gen_int.size()),
+        t_unit=1,
+    )
+    latent_infections = Infections()
+    observed_infections = PoissonObservation()
+    rt = RtRandomWalkProcess(
+        Rt0_dist=dist.TruncatedNormal(loc=1.2, scale=0.2, low=0),
+        Rt_transform=t.ExpTransform().inv,
+        Rt_rw_dist=dist.Normal(0, 0.025),
+    )
+    model = RtInfectionsRenewalModel(
+        I0_rv=I0,
+        gen_int_rv=gen_int,
+        latent_infections_rv=latent_infections,
+        infection_obs_process_rv=observed_infections,
+        Rt_process_rv=rt,
+    )
+
+    n_timepoints_to_simulate = 30
+    n_forecast_points = 10
+    with npro.handlers.seed(rng_seed=np.random.randint(1, 600)):
+        model_sample = model.sample(
+            n_timepoints_to_simulate=n_timepoints_to_simulate
+        )
+
+    model.run(
+        num_warmup=5,
+        num_samples=5,
+        data_observed_infections=model_sample.observed_infections,
+        rng_key=jr.key(54),
+    )
+
+    posterior_predictive_samples = model.posterior_predictive(
+        n_timepoints_to_simulate=n_timepoints_to_simulate + n_forecast_points,
+    )
+
+    # Check the length of the predictive distribution
+    assert (
+        len(posterior_predictive_samples["poisson_rv"][0])
+        == n_timepoints_to_simulate + n_forecast_points
+    )
+
+    # Check the first elements of the posterior predictive Rt are the same as the
+    # posterior Rt
+    assert_array_equal(
+        model.mcmc.get_samples()["Rt"][0],
+        posterior_predictive_samples["Rt"][0][
+            : len(model.mcmc.get_samples()["Rt"][0])
+        ],
+    )
--- model/src/test/test_model_hospitalizations.py
+++ model/src/test/test_model_hospitalizations.py
@@ -253,13 +253,13 @@ def test_model_hosp_no_obs_model():
     with npro.handlers.seed(rng_seed=np.random.randint(1, 600)):
         model0_samp = model0.sample(n_timepoints_to_simulate=30)
 
-    model0.observation_process = NullObservation()
+    model0.hosp_admission_obs_process_rv = NullObservation()
 
     np.random.seed(223)
     with npro.handlers.seed(rng_seed=np.random.randint(1, 600)):
         model1_samp = model0.sample(n_timepoints_to_simulate=30)
 
-    np.testing.assert_array_equal(model0_samp.Rt, model1_samp.Rt)
+    np.testing.assert_array_almost_equal(model0_samp.Rt, model1_samp.Rt)
     np.testing.assert_array_equal(
         model0_samp.latent_infections, model1_samp.latent_infections
     )
@@ -493,6 +493,7 @@ def test_model_hosp_with_obs_model_weekday_phosp():
         jnp.array([0.25, 0.25, 0.25, 0.25]), name="gen_int"
     )
     n_obs_to_generate = 30
+    pad_size = 5
 
     I0 = InfectionSeedingProcess(
         "I0_seeding",
@@ -536,17 +537,17 @@ def test_model_hosp_with_obs_model_weekday_phosp():
     )
 
     # Other random components
+    total_length = n_obs_to_generate + pad_size + gen_int.size()
     weekday = jnp.array([1, 1, 1, 1, 2, 2])
     weekday = weekday / weekday.sum()
     weekday = jnp.tile(weekday, 10)
-    # weekday = weekday[:n_obs_to_generate]
-    weekday = weekday[:34]
+    weekday = weekday[:total_length]
 
     weekday = DeterministicVariable(weekday, name="weekday")
 
     hosp_report_prob_dist = jnp.array([0.9, 0.8, 0.7, 0.7, 0.6, 0.4])
     hosp_report_prob_dist = jnp.tile(hosp_report_prob_dist, 10)
-    hosp_report_prob_dist = hosp_report_prob_dist[:34]
+    hosp_report_prob_dist = hosp_report_prob_dist[:total_length]
     hosp_report_prob_dist = hosp_report_prob_dist / hosp_report_prob_dist.sum()
 
     hosp_report_prob_dist = DeterministicVariable(
@@ -572,23 +573,19 @@ def test_model_hosp_with_obs_model_weekday_phosp():
     )
 
     # Sampling and fitting model 0 (with no obs for infections)
+
     np.random.seed(223)
     with npro.handlers.seed(rng_seed=np.random.randint(1, 600)):
-        model1_samp = model1.sample(n_timepoints_to_simulate=n_obs_to_generate)
+        model1_samp = model1.sample(
+            n_timepoints_to_simulate=n_obs_to_generate, padding=pad_size
+        )
 
-    pad_size = 5
-    obs = jnp.hstack(
-        [
-            jnp.repeat(jnp.nan, pad_size),
-            model1_samp.observed_hosp_admissions[pad_size:],
-        ]
-    )
     # Running with padding
     model1.run(
         num_warmup=500,
         num_samples=500,
         rng_key=jr.key(272),
-        data_observed_hosp_admissions=obs,
+        data_observed_hosp_admissions=model1_samp.observed_hosp_admissions,
         padding=pad_size,
     )
 
