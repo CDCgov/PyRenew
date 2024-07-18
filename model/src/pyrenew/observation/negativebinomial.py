85f8de4c73e8a72fdecd2b85bc29672ab91f2eb7
Dylan H. Morris <dylanhmorris@users.noreply.github.com>
2024-07-18 12:33:07 -0400

    Update negativebinomial.py
    Negative binomial obs was returning `tuple[tuple[ArrayLike]]` when we want `tuple[ArrayLike]`

--- model/src/pyrenew/observation/negativebinomial.py
+++ model/src/pyrenew/observation/negativebinomial.py
@@ -89,14 +89,12 @@ class NegativeBinomialObservation(RandomVariable):
         """
         concentration, *_ = self.concentration_rv.sample()
 
-        negative_binomial_sample = (
-            numpyro.sample(
+        negative_binomial_sample = numpyro.sample(
                 name=self.name,
                 fn=dist.NegativeBinomial2(
                     mean=mu + self.eps,
                     concentration=concentration,
                 ),
                 obs=obs,
-            ),
-        )
+            )
         return (negative_binomial_sample,)
