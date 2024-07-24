e8a3fad5b9a25317e40bdaa6accc5395c734800d
damonbayer <xum8@cdc.gov>
2024-07-24 17:37:43 -0500

    load variables from stan_data

---
+++ demo_hosp_only_ww_model.py
@@ -0,0 +1,48 @@
+# numpydoc ignore=GL08
+import json
+
+import numpyro
+import numpyro.distributions as dist
+from pyrenew.metaclass import DistributionalRV
+from pyrenew.model import hosp_only_ww_model
+
+# Load the JSON file
+with open(
+    "scratch/stan_data_hosp_only.json",
+    "r",
+) as file:
+    stan_data = json.load(file)
+
+# Pylance complains about not being able to find variables
+# for key in stan_data:
+#     if isinstance(stan_data[key], (list, tuple)) and len(stan_data[key]) == 1:
+#         globals()[key] = stan_data[key][0]
+#     else:
+#         globals()[key] = stan_data[key]
+
+i0_over_n_prior_a = stan_data["i0_over_n_prior_a"][0]
+i0_over_n_prior_b = stan_data["i0_over_n_prior_b"][0]
+initial_growth_prior_mean = stan_data["initial_growth_prior_mean"][0]
+initial_growth_prior_sd = stan_data["initial_growth_prior_sd"][0]
+uot = stan_data["uot"][0]
+
+i0_over_n_rv = DistributionalRV(
+    dist.Beta(i0_over_n_prior_a, i0_over_n_prior_b), "i0_over_n_rv"
+)
+
+initialization_rate_rv = DistributionalRV(
+    dist.Normal(initial_growth_prior_mean, initial_growth_prior_sd), "rate"
+)
+state_pop = stan_data["state_pop"][0]
+my_model = hosp_only_ww_model(
+    state_pop=state_pop,
+    i0_over_n_rv=i0_over_n_rv,
+    initialization_rate_rv=initialization_rate_rv,
+    n_initialization_points=uot,
+)
+
+with numpyro.handlers.seed(rng_seed=200):
+    my_model_samp = my_model.sample()
+
+
+my_model_samp
