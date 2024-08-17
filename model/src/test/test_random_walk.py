ee64dea64e55efe30228c5a4ddbe150e3ae536bf
Dylan H. Morris <dylan@dylanhmorris.com>
2024-08-17 10:52:52 -0400

    Autoformat files

--- model/src/test/test_random_walk.py
+++ model/src/test/test_random_walk.py
@@ -20,11 +20,9 @@ def test_rw_can_be_sampled():
     )
     init_rv_fixed = DeterministicVariable(name="init_rv_fixed", value=50.0)
 
-
     rw = RandomWalk(
         "rw",
         DistributionalRV(name="rw_step_rv", distribution=dist.Normal(0, 0.27)),
-
     )
 
     with numpyro.handlers.seed(rng_seed=62):
