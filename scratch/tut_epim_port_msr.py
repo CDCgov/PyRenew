eb35bd52527c061ed6d9d3539c7f654ee27dfee1
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-08-23 09:36:02 -0400

    attempt fix rpy2 issues

--- scratch/tut_epim_port_msr.py
+++ scratch/tut_epim_port_msr.py
@@ -23,13 +23,15 @@ import numpy as np
 import numpyro
 import numpyro.distributions as dist
 import polars as pl
-import pyrenew.transformation as t
-import rpy2.robjects as ro
+
+# import rpy2.robjects as ro
 import toml
 from jax.typing import ArrayLike
 from matplotlib import font_manager as fm
 from numpyro import render_model
 from numpyro.infer.reparam import LocScaleReparam
+
+import pyrenew.transformation as t
 from pyrenew.deterministic import DeterministicPMF
 from pyrenew.latent import (
     InfectionInitializationProcess,
@@ -46,8 +48,9 @@ from pyrenew.metaclass import (
 from pyrenew.observation import NegativeBinomialObservation
 from pyrenew.process import SimpleRandomWalkProcess
 from pyrenew.regression import GLMPrediction
-from rpy2.robjects import pandas2ri
-from rpy2.robjects.packages import importr
+
+# from rpy2.robjects import pandas2ri
+# from rpy2.robjects.packages import importr
 
 FONT_PATH = "texgyreschola-regular.otf"
 if os.path.exists(FONT_PATH):
@@ -1694,44 +1697,44 @@ def plot_hdi_arviz_for(idata, forecast_days):  # numpydoc ignore=GL08
     plt.show()
 
 
-def quantilize_forecasts(
-    samples_dict,
-    state_abbr,
-    start_date,
-    end_date,
-    fitting_data,
-    output_path,
-    reference_date,
-):  # numpydoc ignore=GL08
-    pandas2ri.activate()
-    forecasttools = importr("forecasttools")
-    # dplyr = importr("dplyr")
-    # tidyr = importr("tidyr")
-    # cli = importr("cli")
+# def quantilize_forecasts(
+#     samples_dict,
+#     state_abbr,
+#     start_date,
+#     end_date,
+#     fitting_data,
+#     output_path,
+#     reference_date,
+# ):  # numpydoc ignore=GL08
+#     pandas2ri.activate()
+#     forecasttools = importr("forecasttools")
+#     # dplyr = importr("dplyr")
+#     # tidyr = importr("tidyr")
+#     # cli = importr("cli")
 
-    posterior_samples = pl.DataFrame(samples_dict)
-    posterior_samples_pd = posterior_samples.to_pandas()
-    r_posterior_samples = pandas2ri.py2rpy(posterior_samples_pd)
+#     posterior_samples = pl.DataFrame(samples_dict)
+#     posterior_samples_pd = posterior_samples.to_pandas()
+#     r_posterior_samples = pandas2ri.py2rpy(posterior_samples_pd)
 
-    fitting_data_pd = fitting_data.to_pandas()
-    r_fitting_data = pandas2ri.py2rpy(fitting_data_pd)
+#     fitting_data_pd = fitting_data.to_pandas()
+#     r_fitting_data = pandas2ri.py2rpy(fitting_data_pd)
 
-    results_list = ro.ListVector({state_abbr: r_posterior_samples})
+#     results_list = ro.ListVector({state_abbr: r_posterior_samples})
 
-    horizons = ro.IntVector([-1, 0, 1, 2, 3])
+#     horizons = ro.IntVector([-1, 0, 1, 2, 3])
 
-    forecast_output = forecasttools.forecast_and_output_flusight(
-        data=r_fitting_data,
-        results=results_list,
-        output_path=output_path,
-        reference_date=reference_date,
-        horizons=horizons,
-        seed=62352,
-    )
+#     forecast_output = forecasttools.forecast_and_output_flusight(
+#         data=r_fitting_data,
+#         results=results_list,
+#         output_path=output_path,
+#         reference_date=reference_date,
+#         horizons=horizons,
+#         seed=62352,
+#     )
 
-    forecast_output_pd = pandas2ri.rpy2py(forecast_output)
-    forecast_output_pl = pl.from_pandas(forecast_output_pd)
-    print(forecast_output_pl)
+#     forecast_output_pd = pandas2ri.rpy2py(forecast_output)
+#     forecast_output_pl = pl.from_pandas(forecast_output_pd)
+#     print(forecast_output_pl)
 
 
 def main(args):  # numpydoc ignore=GL08
