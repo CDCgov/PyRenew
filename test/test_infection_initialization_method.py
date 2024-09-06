21ab14c59e9ba09f2ea442383f63aaf338a8a435
damonbayer <xum8@cdc.gov>
2024-09-06 15:18:29 -0500

    make test multidimensional

--- test/test_infection_initialization_method.py
+++ test/test_infection_initialization_method.py
@@ -14,44 +14,75 @@ from pyrenew.latent import (
 def test_initialize_infections_exponential():
     """Check that the InitializeInfectionsExponentialGrowth class generates the correct number of infections at each time point."""
     n_timepoints = 10
-    rate_RV = DeterministicVariable(name="rate_RV", value=0.5)
-    I_pre_init_RV = DeterministicVariable(name="I_pre_init_RV", value=10.0)
+    rate_RV = DeterministicVariable(name="rate_RV", value=np.array([0.5, 0.1]))
+    I_pre_init_RV = DeterministicVariable(
+        name="I_pre_init_RV", value=np.array([5.0, 10.0])
+    )
     default_t_pre_init = n_timepoints - 1
 
-    (I_pre_init,) = I_pre_init_RV()
-    (rate,) = rate_RV()
+    I_pre_init = I_pre_init_RV()[0].value
+    rate = rate_RV()[0].value
 
-    I_pre_init = I_pre_init.value
-    rate = rate.value
     infections_default_t_pre_init = InitializeInfectionsExponentialGrowth(
         n_timepoints, rate_rv=rate_RV
     ).initialize_infections(I_pre_init)
-    infections_default_t_pre_init_manual = I_pre_init * np.exp(
-        rate * (np.arange(n_timepoints) - default_t_pre_init)
+
+    infections_default_t_pre_init_manual_0 = I_pre_init[0] * np.exp(
+        rate[0] * (np.arange(n_timepoints) - default_t_pre_init)
+    )
+
+    infections_default_t_pre_init_manual_1 = I_pre_init[1] * np.exp(
+        rate[1] * (np.arange(n_timepoints) - default_t_pre_init)
+    )
+
+    infections_default_t_pre_init_manual = np.column_stack(
+        [
+            infections_default_t_pre_init_manual_0,
+            infections_default_t_pre_init_manual_1,
+        ]
     )
 
     testing.assert_array_almost_equal(
         infections_default_t_pre_init, infections_default_t_pre_init_manual
     )
 
+    # test the one dimensional
+
     # assert that infections at default t_pre_init is I_pre_init
-    assert infections_default_t_pre_init[default_t_pre_init] == I_pre_init
+    testing.assert_array_equal(
+        infections_default_t_pre_init[default_t_pre_init], I_pre_init
+    )
 
     # test non-default t_pre_init
     t_pre_init = 6
     infections_custom_t_pre_init = InitializeInfectionsExponentialGrowth(
         n_timepoints, rate_rv=rate_RV, t_pre_init=t_pre_init
     ).initialize_infections(I_pre_init)
-    infections_custom_t_pre_init_manual = I_pre_init * np.exp(
-        rate * (np.arange(n_timepoints) - t_pre_init)
+
+    infections_custom_t_pre_init_manual_0 = I_pre_init[0] * np.exp(
+        rate[0] * (np.arange(n_timepoints) - t_pre_init)
     )
+
+    infections_custom_t_pre_init_manual_1 = I_pre_init[1] * np.exp(
+        rate[1] * (np.arange(n_timepoints) - t_pre_init)
+    )
+
+    infections_custom_t_pre_init_manual = np.column_stack(
+        [
+            infections_custom_t_pre_init_manual_0,
+            infections_custom_t_pre_init_manual_1,
+        ]
+    )
+
     testing.assert_array_almost_equal(
         infections_custom_t_pre_init,
         infections_custom_t_pre_init_manual,
         decimal=5,
     )
 
-    assert infections_custom_t_pre_init[t_pre_init] == I_pre_init
+    testing.assert_array_equal(
+        infections_custom_t_pre_init[t_pre_init], I_pre_init
+    )
 
 
 def test_initialize_infections_zero_pad():
