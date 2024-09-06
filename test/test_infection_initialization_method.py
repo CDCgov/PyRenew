3c5fbe72b611cfbfa0c3e9da6f0199ab51213717
Damon Bayer <xum8@cdc.gov>
2024-09-06 15:48:12 -0500

    Reworks `InitializeInfectionsExponentialGrowth` to work with `numpyro.plate` (#432)

--- test/test_infection_initialization_method.py
+++ test/test_infection_initialization_method.py
@@ -14,64 +14,116 @@ from pyrenew.latent import (
 def test_initialize_infections_exponential():
     """Check that the InitializeInfectionsExponentialGrowth class generates the correct number of infections at each time point."""
     n_timepoints = 10
-    rate_RV = DeterministicVariable(name="rate_RV", value=0.5)
-    I_pre_init_RV = DeterministicVariable(name="I_pre_init_RV", value=10.0)
     default_t_pre_init = n_timepoints - 1
-
-    (I_pre_init,) = I_pre_init_RV()
-    (rate,) = rate_RV()
-
-    I_pre_init = I_pre_init.value
-    rate = rate.value
-    infections_default_t_pre_init = InitializeInfectionsExponentialGrowth(
-        n_timepoints, rate=rate_RV
-    ).initialize_infections(I_pre_init)
-    infections_default_t_pre_init_manual = I_pre_init * np.exp(
-        rate * (np.arange(n_timepoints) - default_t_pre_init)
-    )
-
-    testing.assert_array_almost_equal(
-        infections_default_t_pre_init, infections_default_t_pre_init_manual
-    )
-
-    # assert that infections at default t_pre_init is I_pre_init
-    assert infections_default_t_pre_init[default_t_pre_init] == I_pre_init
-
-    # test for failure with non-scalar rate or I_pre_init
-    rate_RV_2 = DeterministicVariable(
-        name="rate_RV", value=np.array([0.5, 0.5])
-    )
-    with pytest.raises(ValueError):
-        InitializeInfectionsExponentialGrowth(
-            n_timepoints, rate=rate_RV_2
-        ).initialize_infections(I_pre_init)
-
-    I_pre_init_RV_2 = DeterministicVariable(
-        name="I_pre_init_RV",
-        value=np.array([10.0, 10.0]),
-    )
-    (I_pre_init_2,) = I_pre_init_RV_2()
-
-    with pytest.raises(ValueError):
-        InitializeInfectionsExponentialGrowth(
-            n_timepoints, rate=rate_RV
-        ).initialize_infections(I_pre_init_2.value)
-
-    # test non-default t_pre_init
     t_pre_init = 6
-    infections_custom_t_pre_init = InitializeInfectionsExponentialGrowth(
-        n_timepoints, rate=rate_RV, t_pre_init=t_pre_init
+
+    rate_RV = DeterministicVariable(name="rate_RV", value=np.array([0.5, 0.1]))
+    rate_scalar_RV = DeterministicVariable(name="rate_RV", value=0.5)
+
+    rate = rate_RV()[0].value
+    rate_scalar = rate_scalar_RV()[0].value
+
+    I_pre_init = np.array([5.0, 10.0])
+    I_pre_init_scalar = 5.0
+
+    # both rate and I_pre_init are arrays with default t_pre_init
+    result = InitializeInfectionsExponentialGrowth(
+        n_timepoints, rate_rv=rate_RV
     ).initialize_infections(I_pre_init)
-    infections_custom_t_pre_init_manual = I_pre_init * np.exp(
-        rate * (np.arange(n_timepoints) - t_pre_init)
-    )
-    testing.assert_array_almost_equal(
-        infections_custom_t_pre_init,
-        infections_custom_t_pre_init_manual,
-        decimal=5,
+
+    manual_result = np.column_stack(
+        [
+            I_pre_init[0]
+            * np.exp(rate[0] * (np.arange(n_timepoints) - default_t_pre_init)),
+            I_pre_init[1]
+            * np.exp(rate[1] * (np.arange(n_timepoints) - default_t_pre_init)),
+        ]
     )
 
-    assert infections_custom_t_pre_init[t_pre_init] == I_pre_init
+    ## check that the result is as expected
+    testing.assert_array_almost_equal(result, manual_result)
+
+    ## check that infections at default t_pre_init is I_pre_init
+    testing.assert_array_equal(result[default_t_pre_init], I_pre_init)
+
+    # both rate and I_pre_init are arrays with custom t_pre_init
+    result = InitializeInfectionsExponentialGrowth(
+        n_timepoints, rate_rv=rate_RV, t_pre_init=t_pre_init
+    ).initialize_infections(I_pre_init)
+
+    manual_result = np.column_stack(
+        [
+            I_pre_init[0]
+            * np.exp(rate[0] * (np.arange(n_timepoints) - t_pre_init)),
+            I_pre_init[1]
+            * np.exp(rate[1] * (np.arange(n_timepoints) - t_pre_init)),
+        ]
+    )
+
+    ## check that the result is as expected
+    testing.assert_array_almost_equal(result, manual_result)
+
+    ## check that infections at t_pre_init is I_pre_init
+    testing.assert_array_equal(result[t_pre_init], I_pre_init)
+
+    # rate is array, I_pre_init is scalar with default t_pre_init
+    result = InitializeInfectionsExponentialGrowth(
+        n_timepoints, rate_rv=rate_RV
+    ).initialize_infections(I_pre_init_scalar)
+
+    manual_result = np.column_stack(
+        [
+            I_pre_init_scalar
+            * np.exp(rate[0] * (np.arange(n_timepoints) - default_t_pre_init)),
+            I_pre_init_scalar
+            * np.exp(rate[1] * (np.arange(n_timepoints) - default_t_pre_init)),
+        ]
+    )
+
+    ## check that the result is as expected
+    testing.assert_array_almost_equal(result, manual_result)
+
+    ## check that infections at default t_pre_init is I_pre_init
+    testing.assert_array_equal(result[default_t_pre_init], I_pre_init_scalar)
+
+    # rate is scalar, I_pre_init is array with default t_pre_init
+    result = InitializeInfectionsExponentialGrowth(
+        n_timepoints, rate_rv=rate_scalar_RV
+    ).initialize_infections(I_pre_init)
+
+    manual_result = np.column_stack(
+        [
+            I_pre_init[0]
+            * np.exp(
+                rate_scalar * (np.arange(n_timepoints) - default_t_pre_init)
+            ),
+            I_pre_init[1]
+            * np.exp(
+                rate_scalar * (np.arange(n_timepoints) - default_t_pre_init)
+            ),
+        ]
+    )
+
+    ## check that the result is as expected
+    testing.assert_array_almost_equal(result, manual_result)
+
+    ## check that infections at default t_pre_init is I_pre_init
+    testing.assert_array_equal(result[default_t_pre_init], I_pre_init)
+
+    # both rate and I_pre_init are scalar with default t_pre_init
+    result = InitializeInfectionsExponentialGrowth(
+        n_timepoints, rate_rv=rate_scalar_RV
+    ).initialize_infections(I_pre_init_scalar)
+
+    manual_result = I_pre_init_scalar * np.exp(
+        rate_scalar * (np.arange(n_timepoints) - default_t_pre_init)
+    )
+
+    ## check that the result is as expected
+    testing.assert_array_almost_equal(result, manual_result)
+
+    ## check that infections at default t_pre_init is I_pre_init
+    testing.assert_array_equal(result[default_t_pre_init], I_pre_init_scalar)
 
 
 def test_initialize_infections_zero_pad():
