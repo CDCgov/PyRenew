396c2d70dbc518248d7218d3f630e822bf71534f
sbidari <zib2@cdc.gov>
2024-09-12 11:54:01 -0400

    remove tests

--- test/test_arrayutils.py
+++ test/test_arrayutils.py
@@ -8,58 +8,6 @@ import pytest
 import pyrenew.arrayutils as au
 
 
-def test_arrayutils_pad_to_match():
-    """
-    Verifies extension when required and error when `fix_y` is True.
-    """
-
-    x = jnp.array([1, 2, 3])
-    y = jnp.array([1, 2])
-
-    x_pad, y_pad = au.pad_to_match(x, y)
-
-    assert x_pad.size == y_pad.size
-    assert x_pad.size == 3
-
-    x = jnp.array([1, 2])
-    y = jnp.array([1, 2, 3])
-
-    x_pad, y_pad = au.pad_to_match(x, y)
-
-    assert x_pad.size == y_pad.size
-    assert x_pad.size == 3
-
-    x = jnp.array([1, 2, 3])
-    y = jnp.array([1, 2])
-
-    # Verify that the function raises an error when `fix_y` is True
-    with pytest.raises(ValueError):
-        x_pad, y_pad = au.pad_to_match(x, y, fix_y=True)
-
-    # Verify function works with both padding directions
-    x_pad, y_pad = au.pad_to_match(x, y, pad_direction="start")
-
-    assert x_pad.size == y_pad.size
-    assert x_pad.size == 3
-
-    # Verify function raises an error when pad_direction is not "start" or "end"
-    with pytest.raises(ValueError):
-        x_pad, y_pad = au.pad_to_match(x, y, pad_direction="middle")
-
-
-def test_arrayutils_pad_x_to_match_y():
-    """
-    Verifies extension when required
-    """
-
-    x = jnp.array([1, 2])
-    y = jnp.array([1, 2, 3])
-
-    x_pad = au.pad_x_to_match_y(x, y)
-
-    assert x_pad.size == 3
-
-
 def test_pad_edges_to_match():
     """
     Test function to verify padding along the edges for 1D and 2D arrays
