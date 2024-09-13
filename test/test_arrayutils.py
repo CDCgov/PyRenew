0cd56ad169a4d39941a24f5885fc9e34b06bc5f0
sbidari <zib2@cdc.gov>
2024-09-13 16:45:39 -0400

    code review suggestion

--- test/test_arrayutils.py
+++ test/test_arrayutils.py
@@ -20,6 +20,7 @@ def test_pad_edges_to_match():
     x_pad, y_pad = au.pad_edges_to_match(x, y)
     assert x_pad.size == y_pad.size
     assert y_pad[-1] == y[-1]
+    assert jnp.array_equal(x_pad, x)
 
     # test when x gets padded
     x = jnp.array([1, 2])
@@ -28,6 +29,7 @@ def test_pad_edges_to_match():
     x_pad, y_pad = au.pad_edges_to_match(x, y)
     assert x_pad.size == y_pad.size
     assert x_pad[-1] == x[-1]
+    assert jnp.array_equal(y_pad, y)
 
     # test when no padding required
     x = jnp.array([1, 2, 3])
@@ -45,6 +47,7 @@ def test_pad_edges_to_match():
 
     assert x_pad.size == y_pad.size
     assert y_pad[0] == y[0]
+    assert jnp.array_equal(x_pad, x)
 
     # Verify that the function raises an error when `fix_y` is True
     with pytest.raises(
@@ -66,9 +69,11 @@ def test_pad_edges_to_match():
 
     assert jnp.array_equal(x_pad.shape[axis], y_pad.shape[axis])
     assert jnp.array_equal(y_pad[-1], y[-1])
+    assert jnp.array_equal(x_pad, x)
 
     # padding along axis 1
     axis = 1
     x_pad, y_pad = au.pad_edges_to_match(x, y, axis=axis, pad_direction="end")
     assert jnp.array_equal(x_pad.shape[axis], y_pad.shape[axis])
     assert jnp.array_equal(y[:, -1], y_pad[:, -1])
+    assert jnp.array_equal(x_pad, x)
