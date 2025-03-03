d275f2102212ee6352f31851fda165adb42122a6
Dylan H. Morris <dylanhmorris@users.noreply.github.com>
2025-03-03 12:33:05 -0500

    Add weekly_to_daily functionality, move time handling functions to their own module (#483)

---
+++ pyrenew/time.py
@@ -0,0 +1,262 @@
+"""
+Helper functions for handling timeseries in Pyrenew
+
+Days of the week in pyrenew are 0-indexed and follow
+ISO standards, so 0 is Monday at 6 is Sunday.
+"""
+
+import jax.numpy as jnp
+from jax.typing import ArrayLike
+
+
+def validate_dow(day_of_week: int, variable_name: str) -> None:
+    """
+    Confirm that an integer is a valid Pyrenew day of the week
+    index, with informative error messages on failure.
+
+    Parameters
+    ----------
+    day_of_week: int
+       Integer to validate.
+
+    variable_name: str
+       Name of the variable being validated, to increase
+       the informativeness of the error message.
+
+    Returns
+    -------
+    None
+       If validation passes.
+
+    Raises
+    ------
+    ValueError
+       If validation fails.
+    """
+    if not isinstance(day_of_week, int):
+        raise ValueError(
+            "Day-of-week indices must be integers "
+            "between 0 and 6, inclusive. "
+            f"Got {day_of_week} for {variable_name}, "
+            "which is a "
+            f"{type(day_of_week)}"
+        )
+    if day_of_week < 0 or day_of_week > 6:
+        raise ValueError(
+            "Day-of-week indices must be a integers "
+            "between 0 and 6, inclusive. "
+            f"Got {day_of_week} for {variable_name}."
+        )
+    return None
+
+
+def daily_to_weekly(
+    daily_values: ArrayLike,
+    input_data_first_dow: int = 0,
+    week_start_dow: int = 0,
+) -> ArrayLike:
+    """
+    Aggregate daily values (e.g.
+    incident hospital admissions)
+    to weekly total values.
+
+    Parameters
+    ----------
+    daily_values : ArrayLike
+        Daily timeseries values (e.g. incident infections or
+        incident ed visits).
+    input_data_first_dow : int
+        First day of the week in the input timeseries `daily_values`.
+        An integer between 0 and 6, inclusive (0 for Monday, 1 for Tuesday,
+        ..., 6 for Sunday).
+        If `input_data_first_dow` does not match `week_start_dow`, the
+        incomplete first week is ignored and weekly values starting
+        from the second week are returned. Defaults to 0.
+    week_start_dow : int
+        Day of the week on which weeks are considered to
+        start in the output timeseries of weekly values
+        (e.g. ISO weeks start on Mondays and end on Sundays;
+        MMWR epiweeks start on Sundays and end on Saturdays).
+        An integer between 0 and 6, inclusive (0 for Monday,
+        1 for Tuesday, ..., 6 for Sunday).
+        Default 0 (i.e. ISO weeks, starting on Mondays).
+
+    Returns
+    -------
+    ArrayLike
+        Data converted to weekly values starting
+        with the first full week available.
+
+    Raises
+    ------
+    ValueError
+        If the specified days of the week fail validation.
+
+    Notes
+    -----
+    This is _not_ a simple inverse of :func:`weekly_to_daily`.
+    This function aggregates (by summing) daily values to
+    create a timeseries of weekly total values.
+    :func:`weekly_to_daily` broadcasts a _single shared value_
+    for a given week as the (repeated) daily value for each day
+    of that week.
+    """
+
+    validate_dow(input_data_first_dow, "input_data_first_dow")
+    validate_dow(week_start_dow, "week_start_dow")
+
+    offset = (week_start_dow - input_data_first_dow) % 7
+    daily_values = daily_values[offset:]
+
+    if daily_values.shape[0] < 7:
+        raise ValueError("No complete weekly values available")
+
+    n_weeks = daily_values.shape[0] // 7
+    trimmed = daily_values[: n_weeks * 7]
+    weekly_values = trimmed.reshape(n_weeks, 7, *daily_values.shape[1:]).sum(
+        axis=1
+    )
+
+    return weekly_values
+
+
+def daily_to_mmwr_epiweekly(
+    daily_values: ArrayLike,
+    input_data_first_dow: int = 6,
+) -> ArrayLike:
+    """
+    Aggregate daily values to weekly values
+    using :func:`daily_to_weekly` with
+    MMWR epidemiological weeks (begin on Sundays,
+    end on Saturdays).
+
+    Parameters
+    ----------
+    daily_values : ArrayLike
+        Daily timeseries values.
+    input_data_first_dow : int
+        First day of the week in the input timeseries `daily_values`.
+        An integer between 0 and 6, inclusive (0 for Monday, 1 for
+        Tuesday, ..., 6 for Sunday).
+        If `input_data_first_dow` is _not_ the MMWR epiweek start day
+        (6, Sunday), the incomplete first week is ignored and
+        weekly values starting from the second week are returned.
+        Defaults to 6 (Sunday).
+
+    Returns
+    -------
+    ArrayLike
+        Data converted to epiweekly values.
+    """
+    return daily_to_weekly(
+        daily_values, input_data_first_dow, week_start_dow=6
+    )
+
+
+def weekly_to_daily(
+    weekly_values: ArrayLike,
+    week_start_dow: int = 0,
+    output_data_first_dow: int = None,
+) -> ArrayLike:
+    """
+    Broadcast a weekly timeseries to a daily
+    timeseries. The value for the week will be used
+    as the value each day in that week, via
+    :func:`jnp.repeat`.
+
+    Parameters
+    ----------
+    weekly_values: ArrayLike
+        Timeseries of weekly values, where
+        (discrete) time is the first dimension of
+        the array (following Pyrenew convention).
+
+    week_start_dow: int
+        Day of the week on which weeks are considered to
+        start in the input ``weekly_values`` timeseries
+        (e.g. ISO weeks start on Mondays and end on Sundays;
+        MMWR epiweeks start on Sundays and end on Saturdays).
+        An integer between 0 and 6, inclusive (0 for Monday,
+        1 for Tuesday, ..., 6 for Sunday).
+        Default 0 (i.e. ISO weeks, starting on Mondays).
+
+    output_data_first_dow: int
+        Day of the week on which to start the output timeseries.
+        An integer between 0 and 6, inclusive (0 for Monday,
+        1 for Tuesday, ..., 6 for Sunday). Defaults to the week
+        start date as specified by ``week_start_dow``.
+        If ``output_data_first_dow`` is _not_ equal to ``week_start_dow``,
+        the first weekly value will be partial (i.e. represented by
+        between 1 and 6 entries in the output timeseries) and
+        all subsequent weeks will be complete (represented by 7
+        values each).
+
+    Returns
+    -------
+    ArrayLike
+        The daily timeseries.
+
+    Raises
+    ------
+    ValueError
+        If the specified days of the week fail validation.
+
+    Notes
+    -----
+    This is _not_ a simple inverse of :func:`daily_to_weekly`.
+    :func:`daily_to_weekly` aggregates (by summing) daily values to
+    create a timeseries of weekly total values.
+    This function broadcasts a _single shared value_
+    for a given week as the (repeated) daily value for each day
+    of that week.
+    """
+
+    validate_dow(week_start_dow, "week_start_dow")
+    if output_data_first_dow is None:
+        output_data_first_dow = week_start_dow
+    validate_dow(output_data_first_dow, "output_data_first_dow")
+
+    offset = (output_data_first_dow - week_start_dow) % 7
+    return jnp.repeat(
+        weekly_values,
+        repeats=7,
+        axis=0,
+    )[offset:]
+
+
+def mmwr_epiweekly_to_daily(
+    weekly_values: ArrayLike,
+    output_data_first_dow: int = 6,
+) -> ArrayLike:
+    """
+    Convert an MMWR epiweekly timeseries to a daily
+    timeseries using :func:`weekly_to_daily`.
+
+    Parameters
+    ----------
+    weekly_values: ArrayLike
+        Timeseries of weekly values, where
+        (discrete) time is the first dimension of
+        the array (following Pyrenew convention).
+
+    output_data_first_dow: int
+        Day of the week on which to start the output timeseries.
+        An integer between 0 and 6, inclusive (0 for Monday,
+        1 for Tuesday, ..., 6 for Sunday). Defaults to the MMWR
+        epiweek start day (6, Sunday).
+        If ``output_data_first_dow`` is _not_ equal to 6 (Sunday,
+        the start of an MMWR epiweek), the first weekly value will
+        be partial (i.e. represented by between 1 and 6 entries
+        in the output timeseries) and all subsequent weeks will be
+        complete (represented by 7 values each).
+
+    Returns
+    -------
+    ArrayLike
+        The daily timeseries.
+    """
+    return weekly_to_daily(
+        weekly_values,
+        output_data_first_dow=output_data_first_dow,
+        week_start_dow=6,
+    )
---
+++ test/test_time.py
@@ -0,0 +1,255 @@
+"""
+Tests for the pyrenew.time module.
+"""
+
+import itertools
+
+import jax.numpy as jnp
+import pytest
+from numpy.testing import assert_array_equal
+
+import pyrenew.time as ptime
+
+
+def test_validate_dow() -> None:
+    """
+    Test that validate_dow raises appropriate
+    errors and succeeds on valid values.
+    """
+    with pytest.raises(ValueError, match="which is a"):
+        ptime.validate_dow("a string", "var")
+    with pytest.raises(ValueError, match="Got -1 for var"):
+        ptime.validate_dow(-1, "var")
+    with pytest.raises(ValueError, match="Got 7 for other_var"):
+        ptime.validate_dow(7, "other_var")
+    assert [ptime.validate_dow(x, "valid") for x in range(7)] == [None] * 7
+
+
+def test_daily_to_weekly_no_offset():
+    """
+    Tests that the function correctly aggregates
+    daily values into weekly totals when there
+    is no offset both input and output start dow on Monday.
+    """
+    daily_values = jnp.arange(1, 15)
+    result = ptime.daily_to_weekly(daily_values)
+    expected = jnp.array([28, 77])
+    assert jnp.array_equal(result, expected)
+
+
+def test_daily_to_weekly_with_input_data_offset():
+    """
+    Tests that the function correctly aggregates
+    daily values into weekly totals with dow
+    offset in the input data.
+    """
+    daily_values = jnp.arange(1, 15)
+    result = ptime.daily_to_weekly(daily_values, input_data_first_dow=2)
+    expected = jnp.array([63])
+    assert jnp.array_equal(result, expected)
+
+
+def test_daily_to_weekly_with_different_week_start():
+    """
+    Tests aggregation when the desired week start
+    differs from the input data start.
+    """
+    daily_values = jnp.arange(1, 15)
+    result = ptime.daily_to_weekly(
+        daily_values, input_data_first_dow=2, week_start_dow=5
+    )
+    expected = jnp.array([49])
+    assert jnp.array_equal(result, expected)
+
+
+def test_daily_to_weekly_incomplete_week():
+    """
+    Tests that the function raises a
+    ValueError when there are
+    insufficient daily values to
+    form a complete week.
+    """
+    daily_values = jnp.arange(1, 5)
+    with pytest.raises(
+        ValueError, match="No complete weekly values available"
+    ):
+        ptime.daily_to_weekly(daily_values, input_data_first_dow=0)
+
+
+def test_daily_to_weekly_missing_daily_values():
+    """
+    Tests that the function correctly
+    aggregates the available daily values
+    into weekly values when there are
+    fewer daily values than required for
+    complete weekly totals in the final week.
+    """
+    daily_values = jnp.arange(1, 10)
+    result = ptime.daily_to_weekly(daily_values, input_data_first_dow=0)
+    expected = jnp.array([28])
+    assert jnp.array_equal(result, expected)
+
+
+def test_daily_to_weekly_invalid_offset():
+    """
+    Tests that the function raises a
+    ValueError when the offset is
+    outside the valid range (0-6).
+    """
+    daily_values = jnp.arange(1, 15)
+    with pytest.raises(
+        ValueError,
+        match="Got -1 for input_data_first_dow",
+    ):
+        ptime.daily_to_weekly(daily_values, input_data_first_dow=-1)
+
+    with pytest.raises(
+        ValueError,
+        match="Got 7 for week_start_dow",
+    ):
+        ptime.daily_to_weekly(daily_values, week_start_dow=7)
+
+
+def test_daily_to_mmwr_epiweekly():
+    """
+    Tests aggregation for MMWR epidemiological week.
+    """
+    daily_values = jnp.arange(1, 15)
+    result = ptime.daily_to_mmwr_epiweekly(daily_values)
+    expected = jnp.array([28, 77])
+    assert jnp.array_equal(result, expected)
+    # for any starting day of the week in the input data
+    # except Sunday (6), the result should throw out partial
+    # first and last weeks
+    for dow in range(6):
+        first_sunday_ind = (6 - dow) % 7
+        minus_last_sunday_ind = (dow - 6) % 7
+        expected_partial = 1.0 * jnp.atleast_1d(
+            jnp.sum(daily_values[first_sunday_ind:-minus_last_sunday_ind])
+        )
+        result_partial = ptime.daily_to_mmwr_epiweekly(
+            daily_values, input_data_first_dow=dow
+        )
+        assert jnp.array_equal(result_partial, expected_partial)
+
+
+@pytest.mark.parametrize(
+    "weekly_values, expected_output",
+    [
+        (
+            jnp.array([7, -8, 501]),
+            jnp.array(
+                [
+                    7,
+                    7,
+                    7,
+                    7,
+                    7,
+                    7,
+                    7,
+                    -8,
+                    -8,
+                    -8,
+                    -8,
+                    -8,
+                    -8,
+                    -8,
+                    501,
+                    501,
+                    501,
+                    501,
+                    501,
+                    501,
+                    501,
+                ]
+            ),
+        ),
+        (
+            jnp.array([[7, 80], [14, 15]]),
+            jnp.array(
+                [
+                    [7, 80],
+                    [7, 80],
+                    [7, 80],
+                    [7, 80],
+                    [7, 80],
+                    [7, 80],
+                    [7, 80],
+                    [14, 15],
+                    [14, 15],
+                    [14, 15],
+                    [14, 15],
+                    [14, 15],
+                    [14, 15],
+                    [14, 15],
+                ]
+            ),
+        ),
+    ],
+)
+def test_weekly_to_daily_output(weekly_values, expected_output):
+    """
+    Test that weekly_to_daily works as expected with full
+    and with partial weeks, and that mmwr_epiweekly_to_daily
+    is equivalent to weekly_to_daily with week_start_dow=6.
+    """
+    assert_array_equal(ptime.weekly_to_daily(weekly_values), expected_output)
+    for out_first_dow, input_start_dow in itertools.product(
+        range(7), range(7)
+    ):
+        expected_offset = (out_first_dow - input_start_dow) % 7
+        assert_array_equal(
+            ptime.weekly_to_daily(
+                weekly_values,
+                output_data_first_dow=out_first_dow,
+                week_start_dow=input_start_dow,
+            ),
+            expected_output[expected_offset:],
+        )
+        if input_start_dow == 6:
+            assert_array_equal(
+                ptime.mmwr_epiweekly_to_daily(
+                    weekly_values, output_data_first_dow=out_first_dow
+                ),
+                expected_output[expected_offset:],
+            )
+
+
+def test_daily_to_weekly_2d_values_with_offset():
+    """
+    Tests that the function correctly aggregates
+    2D daily values into weekly totals with dow
+    offset in the input data.
+    """
+    daily_values_to_broadcast = jnp.arange(1, 4)
+    desired_weeks = 3
+    provided_days = desired_weeks * 7 + 4
+    t_zeros = 2
+
+    daily_values = jnp.broadcast_to(
+        daily_values_to_broadcast,
+        (provided_days, daily_values_to_broadcast.size),
+    )
+
+    daily_values_with_leading_zeros = jnp.concatenate(
+        [jnp.zeros((t_zeros, daily_values_to_broadcast.size)), daily_values],
+        axis=0,
+    )
+
+    result = ptime.daily_to_weekly(daily_values)
+    result_leading_zero_no_offeset = ptime.daily_to_weekly(
+        daily_values_with_leading_zeros
+    )
+    result_leading_zero_offeset = ptime.daily_to_weekly(
+        daily_values_with_leading_zeros, input_data_first_dow=t_zeros
+    )
+    expected = jnp.repeat(
+        7 * daily_values_to_broadcast[jnp.newaxis, :], desired_weeks, axis=0
+    )
+    expected_leading_zero = jnp.array([[5, 10, 15], [7, 14, 21], [7, 14, 21]])
+
+    assert jnp.array_equal(result, expected)
+    assert jnp.array_equal(result_leading_zero_offeset, expected)
+    assert jnp.array_equal(
+        result_leading_zero_no_offeset, expected_leading_zero
+    )
