5aa8c42a0db7dd8dbb83b64ccfb462c805aa7836
Mitzi Morris <bf06@cdc.gov>
2026-02-05 17:53:40 -0500

    remove unused classes and their unit tests

--- pyrenew/latent/__init__.py
+++ pyrenew/latent/__init__.py
@@ -1,6 +1,5 @@
 # numpydoc ignore=GL08
 
-from pyrenew.latent.hospitaladmissions import HospitalAdmissions
 from pyrenew.latent.infection_functions import (
     compute_infections_from_rt,
     compute_infections_from_rt_with_feedback,
@@ -19,7 +18,6 @@ from pyrenew.latent.infections import Infections
 from pyrenew.latent.infectionswithfeedback import InfectionsWithFeedback
 
 __all__ = [
-    "HospitalAdmissions",
     "Infections",
     "logistic_susceptibility_adjustment",
     "compute_infections_from_rt",
--- pyrenew/model/__init__.py
+++ pyrenew/model/__init__.py
@@ -1,6 +1,3 @@
 # numpydoc ignore=GL08
 
-from pyrenew.model.admissionsmodel import HospitalAdmissionsModel
-from pyrenew.model.rtinfectionsrenewalmodel import RtInfectionsRenewalModel
-
-__all__ = ["RtInfectionsRenewalModel", "HospitalAdmissionsModel"]
+__all__: list[str] = []
