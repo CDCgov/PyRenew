d1b621d0b2c62abefe5fc885f874a6e6d4eacf53
Dylan H. Morris <dzl1@cdc.gov>
2026-03-09 14:49:34 -0400

    Remove periodic classes from test_interface_coverage

--- test/test_interface_coverage.py
+++ test/test_interface_coverage.py
@@ -12,8 +12,6 @@ import operator
 import jax.numpy as jnp
 import numpyro.distributions as dist
 import pytest
-from pyrenew.process.periodiceffect import DayOfWeekEffect, PeriodicEffect
-from pyrenew.process.rtperiodicdiffar import RtPeriodicDiffARProcess
 
 from pyrenew.deterministic import (
     DeterministicPMF,
@@ -126,24 +124,6 @@ def _make_hierarchical_normal_noise():
         rv=DistributionalVariable("sd", dist.TruncatedNormal(0.3, 0.15, low=0.1)),
     )
     return HierarchicalNormalNoise(sensor_mode_rv, sensor_sd_rv)
-
-
-def _make_rt_periodic():
-    """
-    Build an RtPeriodicDiffARProcess instance with name "test_rt_periodic".
-
-    Returns
-    -------
-    RtPeriodicDiffARProcess
-    """
-    return RtPeriodicDiffARProcess(
-        name="test_rt_periodic",
-        offset=0,
-        period_size=7,
-        log_rt_rv=DeterministicVariable("log_rt", jnp.array([0.1, 0.2])),
-        autoreg_rv=DeterministicVariable("autoreg", jnp.array([0.7])),
-        periodic_diff_sd_rv=DeterministicVariable("sd", jnp.array([0.1])),
-    )
 
 
 def _make_infections_with_feedback():
@@ -432,31 +412,6 @@ def test_random_variable_rejects_invalid_name(bad_name):
             "test_snrw",
             id="StandardNormalRandomWalk",
         ),
-        pytest.param(
-            PeriodicEffect(
-                name="test_pe",
-                offset=0,
-                quantity_to_broadcast=DeterministicVariable(
-                    "qty", jnp.array([1.0, 2.0])
-                ),
-            ),
-            "test_pe",
-            id="PeriodicEffect",
-        ),
-        pytest.param(
-            DayOfWeekEffect(
-                name="test_dow",
-                offset=0,
-                quantity_to_broadcast=DeterministicVariable("qty", jnp.ones(7)),
-            ),
-            "test_dow",
-            id="DayOfWeekEffect",
-        ),
-        pytest.param(
-            _make_rt_periodic(),
-            "test_rt_periodic",
-            id="RtPeriodicDiffARProcess",
-        ),
         pytest.param(Infections(name="test_inf"), "test_inf", id="Infections"),
         pytest.param(
             _make_infections_with_feedback(),
@@ -548,17 +503,6 @@ def test_hierarchical_infections_name(gen_int_rv):
                 ("process.fundamental_process", "diff_ar1_fundamental"),
             ],
             id="DifferencedAR1",
-        ),
-        pytest.param(
-            _make_rt_periodic(),
-            [
-                ("ar_diff", "test_rt_periodic_diff"),
-                (
-                    "ar_diff.fundamental_process",
-                    "test_rt_periodic_diff_fundamental",
-                ),
-            ],
-            id="RtPeriodicDiffARProcess",
         ),
         pytest.param(
             ProcessRandomWalk(
